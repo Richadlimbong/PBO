b68dfb5fa74f2dcbcad65dd16ec09a3413b74f78
B-22 <richadtunalimbong9@gmail.com>
2023-03-13 10:06:55 +0700

    update 1

---
+++ belajar.py
@@ -0,0 +1,19 @@
+print("saya sedang belajar oop")
+
+#variable 
+
+mahasiswa ='lia'
+print('nama mahasiswa =',mahasiswa)
+print(type(mahasiswa))
+
+angka = 4
+print('nilai dari variable angka =',angka) 
+print(type(angka))
+
+#mencari luas persegi panjang
+#hasil = panjang *lebar
+
+panjang = 5
+lebar = 10
+hasil = panjang*lebar
+print('luas panjang =',hasil)
